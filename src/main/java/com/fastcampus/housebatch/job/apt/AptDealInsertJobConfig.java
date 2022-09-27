e154008bee29ec1b7affdb9adf20aaa9496b3575
sorayeon <jihwan77@gmail.com>
2022-09-27 23:11:32 +0900

    실거래가 배치 - CompositeJobParametersValidator 확인
    lawdCd, yearMonth 유효성검사

--- src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/fastcampus/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -2,15 +2,18 @@ package com.fastcampus.housebatch.job.apt;
 
 import com.fastcampus.housebatch.adapter.ApartmentApiResource;
 import com.fastcampus.housebatch.core.dto.AptDealDto;
-import com.fastcampus.housebatch.job.validator.FilePathParameterValidator;
+import com.fastcampus.housebatch.job.validator.LawdCdParameterValidator;
+import com.fastcampus.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
@@ -18,10 +21,10 @@ import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -35,8 +38,20 @@ public class AptDealInsertJobConfig {
     public Job aptDealInsertJob(Step aptDealInsertStep) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
+                .validator(aptDealJobParameterValidator())
                 .start(aptDealInsertStep)
                 .build();
+    }
+
+
+    private JobParametersValidator aptDealJobParameterValidator() {
+        // --spring.batch.job.names=aptDealInsertJob -yearMonth=2022-07 -lawdCd=41135
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+        validator.setValidators(Arrays.asList(
+                new LawdCdParameterValidator(),
+                new YearMonthParameterValidator()
+        ));
+        return validator;
     }
 
     @JobScope
@@ -55,11 +70,13 @@ public class AptDealInsertJobConfig {
     @StepScope
     @Bean
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
+            @Value("#{jobParameters['yearMonth']}") String yearMonth,
+            @Value("#{jobParameters['lawdCd']}") String lawdCd,
             Jaxb2Marshaller jaxb2Marshaller
     ) {
         return new StaxEventItemReaderBuilder<AptDealDto>()
                 .name("aptDealResourceReader")
-                .resource(apartmentApiResource.getResource("41135", YearMonth.of(2021, 7)))
+                .resource(apartmentApiResource.getResource(lawdCd, YearMonth.parse(yearMonth)))
                 .addFragmentRootElements("item")
                 .unmarshaller(jaxb2Marshaller)
                 .build();
